f1283ee1a6a9f42fb347fb1ec6d2ab883813eafb
José Antonio <delosangelesrodriguezjose@gmail.com>
2021-12-01 12:59:22 +0100

    Second commit

--- starterTestMicroservice/src/main/java/com/everis/bootcamp/controller/IndexController.java
+++ starterTestMicroservice/src/main/java/com/everis/bootcamp/controller/IndexController.java
@@ -3,10 +3,12 @@ package com.everis.bootcamp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import starter.Saludo;
 
+@Controller
 public class IndexController {
 	
 	@Autowired
